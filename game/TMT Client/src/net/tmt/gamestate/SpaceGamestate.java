86e83e3320e7f305b86de0902027cefdc42f01a5
DoubleSteakhouse <double.steakhouse@gmail.com>
2013-10-22 22:13:32 +0200

    #12 added GUI() framewire for SpaceGamestate

--- game/TMT Client/src/net/tmt/gamestate/SpaceGamestate.java
+++ game/TMT Client/src/net/tmt/gamestate/SpaceGamestate.java
@@ -7,10 +7,13 @@ import net.tmt.entity.ControlledSpaceShip;
 import net.tmt.entity.Entity2D;
 import net.tmt.entity.npc.NPCCargoShip;
 import net.tmt.entity.npc.NPCTransporterShip;
+import net.tmt.game.GameEngine;
 import net.tmt.gfx.Graphics;
 import net.tmt.map.World;
 import net.tmt.util.RandomUtil;
 import net.tmt.util.Vector2d;
+
+import org.lwjgl.util.Color;
 
 public class SpaceGamestate extends AbstractGamestate {
 	private static SpaceGamestate	instance	= new SpaceGamestate();
@@ -44,7 +47,10 @@ public class SpaceGamestate extends AbstractGamestate {
 			e.update(delta);
 
 		ship.update(delta);
+
+		updateGUI(delta);
 	}
+
 
 	@Override
 	public void render(final Graphics g) {
@@ -54,6 +60,53 @@ public class SpaceGamestate extends AbstractGamestate {
 			e.render(g);
 
 		ship.render(g);
+
+		renderGUI(g);
+
+	}
+
+	private void updateGUI(final double delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void renderGUI(final Graphics g) {
+
+		int width = GameEngine.WIDTH;
+		int height = GameEngine.HEIGHT;
+
+		g.setColor(Color.CYAN);
+
+		// ressource
+		g.gui().fillRect(0, 0, width * 0.3f, height * 0.05f);
+
+		// minimap
+		g.gui().fillRect(width * 0.85f, 0, width * 0.15f, height * 0.25f);
+
+		// sidebar
+		g.gui().fillRect(0, height * 0.15f, width * 0.04f, height * 0.5f);
+
+		// missions
+		g.gui().fillRect(width * 0.85f, height * 0.3f, width * 0.15f, height * 0.4f);
+
+		// log/chat
+		g.gui().fillRect(0, height * 0.71, width * 0.05f, height * 0.04f);
+		g.gui().fillRect(0, height * 0.75, width * 0.25f, height * 0.25f);
+
+		// weapons
+		g.gui().fillRect(width * 0.3f, height * 0.75f, width * 0.07f, height * 0.1f);
+		g.gui().fillRect(width * 0.3f, height * 0.90f, width * 0.07f, height * 0.1f);
+
+		// states of ship
+		g.gui().fillRect(width * 0.4f, height * 0.75f, width * 0.2f, height * 0.03f);
+		g.gui().fillRect(width * 0.4f, height * 0.8f, width * 0.2f, height * 0.03f);
+		g.gui().fillRect(width * 0.4f, height * 0.85f, width * 0.2f, height * 0.03f);
+		g.gui().fillRect(width * 0.4f, height * 0.90f, width * 0.2f, height * 0.03f);
+		g.gui().fillRect(width * 0.4f, height * 0.95f, width * 0.2f, height * 0.03f);
+
+		// information window
+		g.gui().fillRect(width * 0.75f, height * 0.75f, width * 0.25f, height * 0.25f);
+
 	}
 
 	public ControlledSpaceShip getShip() {

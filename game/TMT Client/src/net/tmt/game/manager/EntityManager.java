12f2cdfc0bdcfe958ed52410135cdefb0be09895
Cyboot <Cyboot@timweb.de>
2013-10-28 10:56:38 +0100

    refactored entitymanager to be more understandable

--- game/TMT Client/src/net/tmt/game/manager/EntityManager.java
+++ game/TMT Client/src/net/tmt/game/manager/EntityManager.java
@@ -25,34 +25,23 @@ import net.tmt.gfx.Graphics;
 public class EntityManager implements Updateable, Renderable {
 	public static final int					LAYER_0_FAR_BACK	= 0;
 	public static final int					LAYER_1_BACK		= 1;
-	public static final int					LAYER_2_MEDIUM		= 2;				// default
+	public static final int					LAYER_2_MEDIUM		= 2;					// default
 	public static final int					LAYER_3_FRONT		= 3;
 	public static final int					LAYER_4_GUI			= 4;
 
+	/** Map<Layer , List<Entity>> */
 	private Map<Integer, List<Entity2D>>	entityMap			= new HashMap<>();
-	private Map<Integer, List<Entity2D>>	entitiesToAdd		= new HashMap<>();
-	private Map<Integer, List<Entity2D>>	entitiesToDelete	= new HashMap<>();
-	private boolean							dirtyRemove			= false;
-	private boolean							dirtyAdd			= false;
+	private List<Entity2D>					collidableEntities	= new ArrayList<>();
+
+	private AddRemove						addremove			= new AddRemove();
 
 	public EntityManager() {
+		// Init Entitymap
 		entityMap.put(LAYER_0_FAR_BACK, new ArrayList<Entity2D>());
 		entityMap.put(LAYER_1_BACK, new ArrayList<Entity2D>());
 		entityMap.put(LAYER_2_MEDIUM, new ArrayList<Entity2D>());
 		entityMap.put(LAYER_3_FRONT, new ArrayList<Entity2D>());
 		entityMap.put(LAYER_4_GUI, new ArrayList<Entity2D>());
-
-		entitiesToAdd.put(LAYER_0_FAR_BACK, new ArrayList<Entity2D>());
-		entitiesToAdd.put(LAYER_1_BACK, new ArrayList<Entity2D>());
-		entitiesToAdd.put(LAYER_2_MEDIUM, new ArrayList<Entity2D>());
-		entitiesToAdd.put(LAYER_3_FRONT, new ArrayList<Entity2D>());
-		entitiesToAdd.put(LAYER_4_GUI, new ArrayList<Entity2D>());
-
-		entitiesToDelete.put(LAYER_0_FAR_BACK, new ArrayList<Entity2D>());
-		entitiesToDelete.put(LAYER_1_BACK, new ArrayList<Entity2D>());
-		entitiesToDelete.put(LAYER_2_MEDIUM, new ArrayList<Entity2D>());
-		entitiesToDelete.put(LAYER_3_FRONT, new ArrayList<Entity2D>());
-		entitiesToDelete.put(LAYER_4_GUI, new ArrayList<Entity2D>());
 	}
 
 	@Override
@@ -65,32 +54,13 @@ public class EntityManager implements Updateable, Renderable {
 				if (e.isAlive()) {
 					e.update(delta, this);
 				} else {
-					entitiesToDelete.get(key).add(e);
-					dirtyRemove = true;
+					addremove.remove(e, key);
 				}
 			}
 		}
 
-		if (dirtyAdd) {
-			for (Entry<Integer, List<Entity2D>> entry : entitiesToAdd.entrySet()) {
-				Integer key = entry.getKey();
-				List<Entity2D> list = entry.getValue();
-
-				entityMap.get(key).addAll(list);
-				list.clear();
-			}
-			dirtyAdd = false;
-		}
-		if (dirtyRemove) {
-			for (Entry<Integer, List<Entity2D>> entry : entitiesToDelete.entrySet()) {
-				Integer key = entry.getKey();
-				List<Entity2D> list = entry.getValue();
-
-				entityMap.get(key).removeAll(list);
-				list.clear();
-			}
-			dirtyRemove = false;
-		}
+		// manages the addition and removal of entities
+		addremove.update();
 	}
 
 	@Override
@@ -124,7 +94,74 @@ public class EntityManager implements Updateable, Renderable {
 	 * @param layer
 	 */
 	public void addEntity(final Entity2D entity, final int layer) {
-		entitiesToAdd.get(layer).add(entity);
-		dirtyAdd = true;
+		addremove.add(entity, layer);
+	}
+
+	/**
+	 * manages the addition and removal of entities
+	 */
+	private class AddRemove {
+		private Map<Integer, List<Entity2D>>	entitiesToAdd		= new HashMap<>();
+		private Map<Integer, List<Entity2D>>	entitiesToDelete	= new HashMap<>();
+		private boolean							dirtyRemove			= false;
+		private boolean							dirtyAdd			= false;
+
+		public AddRemove() {
+			entitiesToAdd.put(LAYER_0_FAR_BACK, new ArrayList<Entity2D>());
+			entitiesToAdd.put(LAYER_1_BACK, new ArrayList<Entity2D>());
+			entitiesToAdd.put(LAYER_2_MEDIUM, new ArrayList<Entity2D>());
+			entitiesToAdd.put(LAYER_3_FRONT, new ArrayList<Entity2D>());
+			entitiesToAdd.put(LAYER_4_GUI, new ArrayList<Entity2D>());
+
+			entitiesToDelete.put(LAYER_0_FAR_BACK, new ArrayList<Entity2D>());
+			entitiesToDelete.put(LAYER_1_BACK, new ArrayList<Entity2D>());
+			entitiesToDelete.put(LAYER_2_MEDIUM, new ArrayList<Entity2D>());
+			entitiesToDelete.put(LAYER_3_FRONT, new ArrayList<Entity2D>());
+			entitiesToDelete.put(LAYER_4_GUI, new ArrayList<Entity2D>());
+		}
+
+		public void remove(final Entity2D e, final int key) {
+			entitiesToDelete.get(key).add(e);
+			dirtyRemove = true;
+		}
+
+		public void add(final Entity2D entity, final int layer) {
+			entitiesToAdd.get(layer).add(entity);
+			dirtyAdd = true;
+		}
+
+		public void update() {
+			// Add Entities to Map/Lists
+			if (dirtyAdd) {
+				for (Entry<Integer, List<Entity2D>> entry : entitiesToAdd.entrySet()) {
+					Integer key = entry.getKey();
+					List<Entity2D> list = entry.getValue();
+
+					for (Entity2D e : list) {
+						// Entity is collidable? --> add to collidable List
+						if (e.isCollisable())
+							collidableEntities.add(e);
+
+						entityMap.get(key).add(e);
+					}
+					list.clear();
+				}
+				dirtyAdd = false;
+			}
+
+			// remove dead Entities
+			if (dirtyRemove) {
+				for (Entry<Integer, List<Entity2D>> entry : entitiesToDelete.entrySet()) {
+					Integer key = entry.getKey();
+					List<Entity2D> list = entry.getValue();
+
+					// remove dead entities from all Lists
+					entityMap.get(key).removeAll(list);
+					collidableEntities.removeAll(list);
+					list.clear();
+				}
+				dirtyRemove = false;
+			}
+		}
 	}
 }

3ba5c811b2cbc29b35597c515d187b1336c5de9e
Cyboot <Cyboot@timweb.de>
2013-11-01 10:07:19 +0100

    #20 added empty Solid-Particle

---
+++ game/TMT Client/src/net/tmt/entity/particle/Solid.java
@@ -0,0 +1,10 @@
+package net.tmt.entity.particle;
+
+import net.tmt.util.Vector2d;
+
+public class Solid extends Particle {
+
+	public Solid(final Vector2d pos, final double lifetime, final double speed, final double roation) {
+		super(pos, lifetime, speed, roation);
+	}
+}

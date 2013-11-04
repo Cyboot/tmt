2ea7ce99d65acada675697759221f97cf90a22d8
DoubleSteakhouse <double.steakhouse@gmail.com>
2013-11-04 16:40:49 +0100

    #22 added ImageView

---
+++ game/TMT Client/src/net/tmt/gui/elements/ImageView.java
@@ -0,0 +1,28 @@
+package net.tmt.gui.elements;
+
+import net.tmt.gfx.Graphics;
+import net.tmt.gfx.Sprite;
+import net.tmt.util.Vector2d;
+
+public class ImageView extends GuiElement {
+
+
+	private Sprite	img;
+
+	public ImageView(final Vector2d pos, final Sprite img) {
+		super(pos, img.getWidth(), img.getHeight());
+		this.img = img;
+	}
+
+	@Override
+	public void update(final double delta) {
+
+	}
+
+	@Override
+	public void render(final Graphics g) {
+		super.render(g);
+		g.onGui().drawSprite(pos, img);
+	}
+
+}

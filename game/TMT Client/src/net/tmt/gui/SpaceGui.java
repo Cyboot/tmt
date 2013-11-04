b59c8e7b52899f73a8d3b4b46cba9f4f7b70cfa9
DoubleSteakhouse <double.steakhouse@gmail.com>
2013-11-04 16:31:25 +0100

    #22 added TextView, refactored ToolTip

--- game/TMT Client/src/net/tmt/gui/SpaceGui.java
+++ game/TMT Client/src/net/tmt/gui/SpaceGui.java
@@ -121,9 +121,11 @@ public class SpaceGui extends Gui {
 
 		if (guiManager.isSet(GUI_HOVER)) {
 			g.onGui().drawText(width * 0.75 + 5, height * 0.75, (String) guiManager.getValue(GUI_HOVER));
-			new ToolTip(Vector2d.tmp1.set(Mouse.getX() + 10 + World.getInstance().getOffset().x, GameEngine.HEIGHT
-					- Mouse.getY() + World.getInstance().getOffset().y), (String) guiManager.getValue(GUI_HOVER))
-					.render(g);
+			ToolTip tmp = new ToolTip(Vector2d.tmp1.set(Mouse.getX() + 10 + World.getInstance().getOffset().x,
+					GameEngine.HEIGHT - Mouse.getY() + World.getInstance().getOffset().y));
+			tmp.setText((String) guiManager.getValue(GUI_HOVER));
+			tmp.render(g);
+
 
 		} else {
 			g.onGui().drawText(width * 0.75 + 5, height * 0.75, "overwrite");
--- game/TMT Client/src/net/tmt/gui/elements/ToolTip.java
+++ game/TMT Client/src/net/tmt/gui/elements/ToolTip.java
@@ -1,51 +1,13 @@
 package net.tmt.gui.elements;
 
-import net.tmt.gfx.Graphics;
 import net.tmt.util.Vector2d;
 
-public class ToolTip extends LeafElement {
-
-	private String		text;
-	private String[]	textBlock;
+public class ToolTip extends TextView {
 
 
-	public ToolTip(final Vector2d pos, final String text) {
+	public ToolTip(final Vector2d pos) {
 		super(pos, 0, 0);
-		this.text = text;
-		setupBorder();
-
+		maxLineLength = 30;
 	}
-
-	private void setupBorder() {
-
-		int txtLength = text.length();
-
-		if (txtLength < 31) {
-			width = txtLength * 7;
-			height = 12;
-			textBlock = new String[] { text };
-		} else {
-			int rows = (text.length() / 30) + 1;
-			width = 210;
-			height = rows * 12;
-			textBlock = new String[rows];
-			for (int i = 0; i < rows - 1; i++) {
-				textBlock[i] = text.substring(i * rows, (i * rows) + 30);
-			}
-			textBlock[textBlock.length - 1] = text.substring((rows - 1) * 30);
-		}
-
-
-	}
-
-	@Override
-	public void render(final Graphics g) {
-		super.render(g);
-		g.setFont(Graphics.Fonts.font_12);
-		for (int i = 0; i < textBlock.length; i++)
-			g.drawText(pos.x, pos.y + 12 * i, textBlock[i]);
-
-	}
-
 
 }

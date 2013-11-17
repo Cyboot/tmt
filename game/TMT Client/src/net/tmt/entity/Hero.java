5a23db8e016b3162e46088219440320013198e3d
Tarek Saier <tareksaier@gmail.com>
2013-11-17 15:09:33 +0100

    #28 functioning rotation. walking animation now only active when actually walking.

--- game/TMT Client/src/net/tmt/entity/Hero.java
+++ game/TMT Client/src/net/tmt/entity/Hero.java
@@ -3,50 +3,60 @@ package net.tmt.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.tmt.entity.component.move.MoveComponent;
+import net.tmt.entity.component.move.RotateComponent;
 import net.tmt.entity.component.other.AnimatedRenderComponent;
 import net.tmt.game.Controls;
+import net.tmt.game.factory.ComponentFactory;
 import net.tmt.game.manager.EntityManager;
 import net.tmt.gfx.Sprite;
 import net.tmt.util.Vector2d;
 
 public class Hero extends Entity2D {
 
-	private double	SPEED	= 128;
-
-	// private double ROTATION_SPEED = 90;
-	// public boolean IS_ROTATE_LEFT = false;
-	// public boolean IS_ROTATE_RIGHT = false;
+	private double					SPEED			= 128;
+	private double					ROTATION_SPEED	= 180;
+	private AnimatedRenderComponent	aniRenCom;
 
 	public Hero(final Vector2d pos) {
 		super(pos);
 		removeAllComponents();
 		List<Sprite> aniSprites = new ArrayList<Sprite>();
-		aniSprites.add(new Sprite("hero_0"));
-		aniSprites.add(new Sprite("hero_1"));
-		addComponent(new AnimatedRenderComponent(aniSprites, 0.2));
-		// addComponent(new RotateComponent(0, ROTATION_SPEED));
+		aniSprites.add(new Sprite("hero_walk_0"));
+		aniSprites.add(new Sprite("hero_walk_1"));
+		aniRenCom = new AnimatedRenderComponent(aniSprites, 0.2);
+		aniRenCom.setPauseFrame(new Sprite("hero_stand"));
+		addComponent(aniRenCom);
+		ComponentFactory.addDefaultMove(this, 0, 0, ROTATION_SPEED);
 	}
 
 	@Override
 	public void update(final EntityManager caller, final double delta) {
-		super.update(caller, delta);
+		// FIXME: MoveComponent's speed is always 0?
+		double s = ((double) getValue(MoveComponent.SPEED));;
+
+		boolean movingF = true;
+		boolean movingB = true;
 
 		if (Controls.pressed(Controls.HERO_UP))
-			pos.y -= delta * SPEED;
+			dispatchValue(MoveComponent.SPEED, SPEED);
+		else
+			movingF = false;
 		if (Controls.pressed(Controls.HERO_DOWN))
-			pos.y += delta * SPEED;
+			dispatchValue(MoveComponent.SPEED, -SPEED);
+		else
+			movingB = false;
 		if (Controls.pressed(Controls.HERO_LEFT))
-			pos.x -= delta * SPEED;
-		// IS_ROTATE_LEFT = true;
-		// else
-		// IS_ROTATE_LEFT = false;
+			dispatchValue(RotateComponent.IS_ROTATE_LEFT, true);
 		if (Controls.pressed(Controls.HERO_RIGHT))
-			pos.x += delta * SPEED;
-		// IS_ROTATE_RIGHT = true;
-		// else
-		// IS_ROTATE_RIGHT = false;
+			dispatchValue(RotateComponent.IS_ROTATE_RIGHT, true);
 
-		// dispatchValue("IS_ROTATE_LEFT", IS_ROTATE_LEFT);
-		// dispatchValue("IS_ROTATE_RIGHT", IS_ROTATE_RIGHT);
+		if (movingF || movingB)
+			aniRenCom.resumeAnimation();
+		else
+			aniRenCom.pauseAnimation();
+
+		super.update(caller, delta);
 	}
+
 }
--- game/TMT Client/src/net/tmt/entity/component/other/AnimatedRenderComponent.java
+++ game/TMT Client/src/net/tmt/entity/component/other/AnimatedRenderComponent.java
@@ -10,19 +10,36 @@ import net.tmt.util.CountdownTimer;
 public class AnimatedRenderComponent extends RenderComponent {
 
 	private List<Sprite>	sprites	= new ArrayList<Sprite>();
+	private Sprite			pauseFrame;
 	private int				currSpriteIndex;
 	private CountdownTimer	timer;
+	private boolean			paused	= false;
 
 	public AnimatedRenderComponent(final List<Sprite> sprites, final double looptime) {
+		setSprite(sprites.get(0));
 		this.sprites = sprites;
 		this.timer = new CountdownTimer(looptime);
 		this.currSpriteIndex = 0;
 	}
 
+	public void pauseAnimation() {
+		paused = true;
+		if (pauseFrame != null)
+			setSprite(pauseFrame);
+	}
+
+	public void resumeAnimation() {
+		paused = false;
+	}
+
+	public void setPauseFrame(final Sprite pauseFrame) {
+		this.pauseFrame = pauseFrame;
+	}
+
 	@Override
 	public void update(final ComponentDispatcher caller, final double delta) {
 		super.update(caller, delta);
-		if (timer.isTimeleft(delta)) {
+		if (timer.isTimeleft(delta) && !paused) {
 			setSprite(sprites.get(currSpriteIndex));
 			currSpriteIndex = (currSpriteIndex + 1) % sprites.size();
 		}
